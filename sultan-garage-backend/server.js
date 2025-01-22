8439879a9e26d5e6605b007aded29241912bcdf5
Allah-Yar <atebarwaxir4455@gmail.com>
2025-01-23 00:20:47 +0500

    again resolved

--- sultan-garage-backend/server.js
+++ sultan-garage-backend/server.js
@@ -98,7 +98,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/dashboard",
+  callbackURL: process.env.GOOGLE_CALLBACK_URL ,
 },
 function(accessToken, refreshToken, profile, done) {
   User.findOrCreate({ 
