a4a442a1794d8d8f20434e7fe8e7ce23329ce3f5
Allah-Yar <atebarwaxir4455@gmail.com>
2025-03-04 16:30:54 +0500

    changes for db

--- sultan-garage-backend/config/db.js
+++ sultan-garage-backend/config/db.js
@@ -6,13 +6,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // const MONGO_URI = process.env.MONGODB_URI;
-const MONGO_URI = process.env.MONGODB_URI
+const MONGO_URL = process.env.MONGODB_URL
 
 
 
 export const db = async () => {
   try {
-    await mongoose.connect( MONGO_URI );
+    await mongoose.connect( MONGO_URL );
     console.log('MongoDB Connected Successfully on port 27017');
     
     mongoose.connection.on('error', err => {
